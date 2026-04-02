e17ca2533363f44f639d31d8eaa2d728ef141635
JacktheGEAT <jacklynch706@gmail.com>
2026-04-02 17:24:48 -0400

    attributes are set up

--- src/Attribute.jsx
+++ src/Attribute.jsx
@@ -2,38 +2,75 @@ import {React, useState } from 'react';
 
 import './App.css'
 
+const intAttributes = ['base_power', 'mana_cost', 'mana_overcharge','exhaust_limit', 'expunge_limit', 'base_amount']
+const floatAttributes = ['power_multiplier', 'cost_multiplier', 'overcharge_multiplier']
+
 const Attribute = ({ id, attributeClass, onRemove }) => {
+  if (attributeClass == '') {return <></>}
+  let input;
+  if (intAttributes.includes(attributeClass)) {
+    input = <input type="number" step="1"/>
+  }
+  else if (floatAttributes.includes(attributeClass)) {
+    input = <input type="number" step='0.1'/>
+  }
+  else if (attributeClass == "mana_curve") {
+    input = (
+    <select>
+
+    </select>
+    )
+  }
+  else if (attributeClass == "consumability") {
+    input = (
+    <select>
+      <option value="nonconsumable">nonconsumable</option>
+      <option value="refreshing">refreshing</option>
+      <option value="consumable">consumable</option>
+    </select>
+    )
+  }
+  else { input = <></>}
   return (
-    <div style={{ border: '1px dotted blue', padding: '10px', margin: '10px' }}>
-      <h3>{attributeClass}</h3>
-      <button className='remove' onClick={() => onRemove(id)}>-</button>
-    </div>
+    <>
+      <div className='attributeBox'>
+        <button className='remove' onClick={() => onRemove(id)}>-</button>
+        <a>{attributeClass}:</a>
+        {input}
+      </div>
+    </>
   );
 };
 
 export default Attributes;
 
 function Attributes() {
-  const [attributeItems, setAttributeItems] = useState([]);
+  const availableAttributes = ['base_power', 'mana_cost', 'mana_overcharge', 'mana_curve', 'exhaust_limit', 'expunge_limit', 'power_multiplier', 'cost_multiplier', 'overcharge_multiplier', 'base_amount', 'consumability' ]
+  const [attributeBox, setAttributeBox] = useState([]);
   const [attributeInput, setAttributeInput] = useState('');
 
   const addAttribute= () => {
     const newAttribute = { id: Date.now(), attributeClass: `${attributeInput}` };
-    setAttributeItems([...attributeItems, newAttribute]);
+    setAttributeBox([...attributeBox, newAttribute]);
   };
-  const removeAttribute = (id) => {
-    setAttributeItems(attributeItems.filter(item => item.id !== id));
+  const removeAttribute = (id, attributeClass) => {
+    setAttributeBox(attributeBox.filter(item => item.id !== id));
   };
   
   return (
     <>
     <div className="row">
       <a >Attributes: </a>
-      <input type='text' value={attributeInput} onChange={e => setAttributeInput(e.target.value)}/>
+      <select  name="class" id="class" onChange={e => setAttributeInput(e.target.value)}>
+        <option value=""></option>
+        {availableAttributes.map(item => (
+          <option value={item}>{item}</option>
+        ))}
+      </select>
       <button className='add' onClick={addAttribute}>+</button>
     </div>
-    <div>
-      {attributeItems.map(item => (
+    <div className='box'>
+      {attributeBox.map(item => (
           <Attribute 
             key={item.id} 
             id={item.id} 
