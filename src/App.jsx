f36fca70a5d1547423446cae07a72da0253c8814
JacktheGEAT <jacklynch706@gmail.com>
2026-04-02 00:00:19 -0400

    started converting the site into the json editor

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,42 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Json generator</h1>
+      <div class="container" className="generator">
+        <div class="row">
+          <a class="leftjson">ID: </a>
+          <input class="rightjson" name="id" id="id"/>
+        </div>
+        <div class="row">
+          <a class="leftjson">Class: </a>
+          <select class="rightjson" name="class" id="class">
+            <option value="base">Base</option>
+            <option value="sigil">Sigil</option>
+            <option value="augment">Augment</option>
+            <option value="status">Status</option>
+            <option value="artifact">Artifact</option>
+          </select>
+        </div>
+        <div class="row">
+          <a class="leftjson">Name: </a>
+          <input class="rightjson" name="name" id="name"/>
+        </div>
+        <div class="row">
+          <a class="leftjson">Rarity: </a>
+          <select class="rightjson" name="rarity" id="rarity">
+            <option value="common">Common</option>
+            <option value="uncommon">Uncommon</option>
+            <option value="rare">Rare</option>
+            <option value="legendary">Legendary</option>
+            <option value="mythical">Mythical</option>
+          </select>
+        </div>
+        <div class="row">
+          <a class="leftjson">Icon: </a>
+          <input class="rightjson" name="texture" id="texture"/>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
